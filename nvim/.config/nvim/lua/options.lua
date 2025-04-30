b4cdeb2551bab3bcca0e0d12ef41591ffaf0d9bb
dnagard <nagarddaniel@gmail.com>
2025-04-30 13:09:57 +0200

    Make nvim config modular

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,58 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt`, `:help option-list`
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a' --Enable mouse mode
+vim.opt.showmode = false --Don't show mode since it's already on statusline
+
+-- Sync clipboard between OS and Neovim
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 15
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
+-- vim: ts=2 sts=2 sw=2 et
